ecf2aa5a8231635bfab237aa26257ab6bf1093ea
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2013-01-19 18:36:58 +0100

    Additional methods for record parsing.

--- include/cintelhex.h
+++ include/cintelhex.h
@@ -1,19 +1,25 @@
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdint.h>
 
 #define IHEX_ERR_INCORRECT_CHECKSUM 0x01
+#define IHEX_ERR_NO_EOF 0x02
+#define IHEX_ERR_PARSE_ERROR 0x03
 
 typedef enum { IHEX_DATA = 0x00, IHEX_END = 0x01, IHEX_ESA = 0x02,
                IHEX_SSA = 0x03, IHEX_ELA = 0x04, IHEX_SLA = 0x05 } ihex_rtype_t;
 
-typedef char* ihex_rdata_t;
+typedef uint8_t* ihex_rdata_t;
+typedef uint8_t  ihex_rlen_t;
+typedef uint8_t  ihex_rchks_t;
 typedef unsigned int ihex_error_t;
-typedef unsigned long ihex_addr_t;
+typedef uint16_t ihex_addr_t;
 
 typedef struct ihex_record {
 	unsigned int ihr_length;
 	ihex_rtype_t ihr_type;
 	ihex_addr_t  ihr_address;
 	ihex_rdata_t ihr_data;
+	ihex_rchks_t ihr_checksum;
 } ihex_record_t;
 
 typedef struct ihex_records {
@@ -30,6 +36,12 @@ ihex_records_t* ihex_from_string(char* data);
 /// Return error code, or 0 if no error occurred.
 ihex_error_t ihex_errno();
 
+/// Checks if a record's checksum is valid.
+int ihex_check_record(ihex_record_t *r);
+
 /// Return error string, or NULL if no error occurred.
 char* ihex_error();
+
+u_int8_t ihex_fromhex8(u_int8_t *input);
+u_int16_t ihex_fromhex16(u_int8_t *input);
 
--- src/ihex_parse.c
+++ src/ihex_parse.c
@@ -1,5 +1,7 @@
 #include "cintelhex.h"
 
+#define IHEX_CHR_RECORDMARK 0x3A
+
 ihex_records_t* ihex_from_file(char* filename)
 {
 	return NULL;
@@ -10,12 +12,81 @@ ihex_records_t* ihex_from_string(char* data)
 	return NULL;
 }
 
+int ihex_parse_single_record(u_int8_t* data, unsigned int length, ihex_record_t* record)
+{
+	u_int i;
+	
+	if (data[0] != IHEX_CHR_RECORDMARK)
+	{
+		return IHEX_ERR_PARSE_ERROR;
+	}
+	
+	//               1         2         3         4
+	// 0 12 3456 78 90123456789012345678901234567890 12
+	// : 10 0100 00 214601360121470136007EFE09D21901 40
+	
+	record->ihr_length   = (ihex_rlen_t)  ihex_fromhex8(data + 1);
+	record->ihr_address  = (ihex_addr_t)  ihex_fromhex16(data + 3);
+	record->ihr_type     = (ihex_rtype_t) ihex_fromhex8(data + 7);
+	record->ihr_checksum = (ihex_rchks_t) ihex_fromhex8(data + 9 + record->ihr_length * 2);
+
+	record->ihr_data    = (ihex_rdata_t)  malloc(record->ihr_length);
+	
+	for (i = 0; i < record->ihr_length; i ++)
+	{
+		record->ihr_data[i] = ihex_fromhex8(data + 9 + i);
+	}
+	
+	if (ihex_check_record(record) != 0)
+	{
+		return IHEX_ERR_INCORRECT_CHECKSUM;
+	}
+	
+	return 0;
+}
+
+int ihex_check_record(ihex_record_t *r)
+{
+	u_int    i;
+	u_int8_t t = 0;
+	
+	t += r->ihr_length + ((r->ihr_address >> 8) & 0xFF) + (r->ihr_address & 0xFF) + r->ihr_type;
+	
+	for (i = 0; i < r->ihr_length; i ++)
+	{
+		t += r->ihr_data[i];
+	}
+	
+	t += r->ihr_checksum;
+	
+	return ((t & 0xFF) == 0) ? 0 : 1;
+}
+
 ihex_error_t ihex_errno()
 {
-	return 0;
+	return -1;
 }
 
 char* ihex_error()
 {
 	return NULL;
+}
+
+inline u_int8_t ihex_fromhex4(u_int8_t i)
+{
+	if      (i >= 0x61 && i <= 0x66) return i - 0x61 + 10;
+	else if (i >= 0x41 && i <= 0x46) return i - 0x41 + 10;
+	else if (i >= 0x30 && i <= 0x39) return i - 0x30;
+	else return 0;
+}
+
+u_int8_t ihex_fromhex8(u_int8_t *i)
+{
+	return (ihex_fromhex4(i[0]) << 4) + ihex_fromhex4(i[1]);
+}
+
+u_int16_t ihex_fromhex16(u_int8_t *i)
+{
+	return (ihex_fromhex4(i[0]) << 12) + (ihex_fromhex4(i[1]) << 8) + 
+	       (ihex_fromhex4(i[2]) << 4) + ihex_fromhex4(i[0]);
 }
