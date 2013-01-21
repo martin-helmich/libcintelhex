90dc62fc63f137dab587e972382022d5bf4b23c0
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2013-01-21 21:16:58 +0100

    Refactored main test program.

--- tests/test_main.c
+++ tests/test_main.c
@@ -1,24 +1,31 @@
 
 #define UNIT_TEST 1
 
+#define DECLARE_AND_INIT_SUITE(name, desc) \
+	CU_pSuite name = CU_add_suite(desc, init_##name, clean_##name); \
+	if (name == NULL) \
+	{ \
+		CU_cleanup_registry(); \
+		return CU_get_error(); \
+	} \
+	add_tests_##name(name);
+	
+#ifndef CU_FAIL_STRING
+#define CU_FAIL_STRING(str) \
+	{ CU_assertImplementation(CU_FALSE, LINE, (str), FILE, "", CU_FALSE); }
+#endif
+
 #include "CUnit/Basic.h"
 #include <CUnit/CUCurses.h>
 #include "test_parse.c"
+#include "test_record.c"
 
 int main(int argc, char* argv[])
 {
-	CU_pSuite pSuite = NULL;
-
  	if (CUE_SUCCESS != CU_initialize_registry()) return CU_get_error();
-
-	pSuite = CU_add_suite("Parsing", init_parsingsuite, clean_parsingsuite);
-	if (NULL == pSuite)
-	{
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
 	
-	add_tests_parsingsuite(pSuite);
+	DECLARE_AND_INIT_SUITE(recordsuite, "Core record model");
+	DECLARE_AND_INIT_SUITE(parsingsuite, "Input file parsing");
 
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	
